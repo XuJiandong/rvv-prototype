4c984ecc0c34326205544791eb8cf3a60b5fe5ad
xjd <xjd@cryptape.com>
2022-03-17 15:58:04 +0800

    Use batch multiplications

--- bn128-example/src/rvv_impl.rs
+++ bn128-example/src/rvv_impl.rs
@@ -1,4 +1,4 @@
-use core::mem::transmute;
+use core::{mem::transmute, slice::from_raw_parts};
 use rvv::rvv_vector;
 use rvv_asm::rvv_asm;
 use rvv_simulator_runtime::Uint;
@@ -46,9 +46,15 @@ pub fn mont_multi(np1: U256, n: U256, x: U256, y: U256, bits: usize) -> U256 {
 }
 
 #[inline(never)]
-pub fn mont_multi_asm(np1: &[U256], n: &[U256], x: &[U256], y: &[U256], res: &mut [U256]) {
+pub fn mont_multi_asm(
+    np1: &[[u128; 2]],
+    n: &[[u128; 2]],
+    x: &[[u128; 2]],
+    y: &[[u128; 2]],
+    res: &mut [[u128; 2]],
+) {
     let len = x.len() as u64;
-    // not loop version
+    // not loop version.
     debug_assert!(len <= 32);
     // registers allocation:
     // x -> v4
@@ -56,19 +62,21 @@ pub fn mont_multi_asm(np1: &[U256], n: &[U256], x: &[U256], y: &[U256], res: &mu
     // np1 -> v12
     // n -> v16
     // m -> v20
-    // t -> v24 = x*y
-    // u -> v24 (512 bits)
-    // high 256 bits part of x*y -> v28
+    // t -> v24 = x*y (512 bits)
+    // narrow(t) -> v4 (replace x, 256 bits)
+    // t3 -> pointer to recent x
+    // t4 -> pointer to recent y
+    // t1 -> avl
+    // t2 -> vl
     unsafe {
         rvv_asm!(
             "mv t1, {len}",
+            "mv t3, {x}",
+            "mv t4, {y}",
+            // "1:",
             "vsetvli t2, t1, e256, m4",
-
-            "mv t0, {x}",
-            "vle256.v v4, (t0)",
-
-            "mv t0, {y}",
-            "vle256.v v8, (t0)",
+            "vle256.v v4, (t3)",
+            "vle256.v v8, (t4)",
 
             "mv t0, {np1}",
             "vle256.v v12, (t0)",
@@ -76,8 +84,9 @@ pub fn mont_multi_asm(np1: &[U256], n: &[U256], x: &[U256], y: &[U256], res: &mu
             "mv t0, {n}",
             "vle256.v v16, (t0)",
 
-            "vmul.vv v24, v4, v8",
-            "vmul.vv v20, v24, v12",
+            "vwmulu.vv v24, v4, v8",
+            "vnsrl.wx v4, v24, x0",
+            "vmul.vv v20, v4, v12",
 
             // vd[i] = +(vs1[i] * vs2[i]) + vd[i]
             // vd: SEW*2, vs1: SEW, vs2: SEW
@@ -87,10 +96,6 @@ pub fn mont_multi_asm(np1: &[U256], n: &[U256], x: &[U256], y: &[U256], res: &mu
             "addi t0, x0, 256",
             "vnsrl.wx v24, v24, t0",
 
-            // high 256 bits of x*y
-            "vmulhu.vv v28, v4, v8",
-            "vadd.vv v24, v24, v28",
-
             // u >= n
             "vmsleu.vv v0, v16, v24",
             // u = u - n
@@ -99,6 +104,17 @@ pub fn mont_multi_asm(np1: &[U256], n: &[U256], x: &[U256], y: &[U256], res: &mu
             "mv t0, {res}",
             "vse256.v v24, (t0)",
 
+            // // avl -= vl
+            // "sub t1, t1, t2",
+            // // check avl == 0, then break
+            // "beq t1, x0, 2f",
+            // // t2 *= 32
+            // "slli t2, t2, 5",
+            // // increase x, y
+            // "add t3, t3, t2",
+            // "add t4, t4, t2",
+            // "beq x0, x0, 1b",
+            // "2:",
             np1 = in (reg) np1.as_ptr(),
             n =  in (reg) n.as_ptr(),
             x = in (reg) x.as_ptr(),
@@ -122,21 +138,26 @@ pub fn mul_reduce_internal(
     inv: u128,
     inv_high: u128,
 ) {
-    let x: U256 = from_u128pair(this);
-    let y: U256 = from_u128pair(by);
-    let n: U256 = from_u128pair(modulus);
-    let np1: U256 = from_u128pair(&[inv, inv_high]);
-
     if cfg!(feature = "use_rvv_vector") {
+        let x: U256 = from_u128pair(this);
+        let y: U256 = from_u128pair(by);
+        let n: U256 = from_u128pair(modulus);
+        let np1: U256 = from_u128pair(&[inv, inv_high]);
         let Uint::<4>(ref result) = mont_multi(np1, n, x, y, 256);
         this[0] = result[0] as u128 | (result[1] as u128) << 64;
         this[1] = result[2] as u128 | (result[3] as u128) << 64;
     } else {
-        let mut result = [U256::from(0)];
-        mont_multi_asm(&[np1], &[n], &[x], &[y], &mut result);
-        let Uint::<4>(ref result) = result[0];
-        this[0] = result[0] as u128 | (result[1] as u128) << 64;
-        this[1] = result[2] as u128 | (result[3] as u128) << 64;
+        unsafe {
+            let mut result = [[0; 2]];
+            mont_multi_asm(
+                &[[inv, inv_high]],
+                from_raw_parts(modulus as *const [u128; 2], 1),
+                from_raw_parts(this as *const [u128; 2], 1),
+                from_raw_parts(by as *const [u128; 2], 1),
+                &mut result,
+            );
+            *this = result[0];
+        }
     }
 }
 
